61d3f6337fc8a9c15f805a6b090cd6821e2e6062
Ptunc <spolentunc@gmail.com>
2022-10-20 16:51:33 +0300

    just commit

---
+++ java16.java
@@ -0,0 +1,14 @@
+package Java16;
+
+import java.util.Scanner;
+
+public class java16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Yıl: ");
+        int yil = input.nextInt();
+        if(yil%4 == 0) System.out.println(yil + " artık yıldır.");
+        else System.out.println(yil + " artık yıl değildir.");
+    }
+}
